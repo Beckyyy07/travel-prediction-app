46851be690390b7fd4b7eef8068ae5319b7a7dfb
andersonvelosocit <anderson.veloso.cit@naturapay.net>
2025-04-08 12:27:06 -0300

    fix/corrigindo schema de pastas

--- backend/database/db_config.py
+++
@@ -1,6 +0,0 @@
-import sqlalchemy
-
-def conectar_ao_banco():
-    # Configuração de conexão
-    engine = sqlalchemy.create_engine("postgresql://usuario:senha@localhost:5432/nomedobanco")
-    return engine
